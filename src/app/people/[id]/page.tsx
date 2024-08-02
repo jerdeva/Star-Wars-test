0e5314ea8df58d43bd32157554c73b1a8bbfddfc
Olena Jerdeva <lena.jerdeva28@gmail.com>
2024-08-02 13:54:11 +0300

    people[id]

--- src/app/people/[id]/page.tsx
+++ src/app/people/[id]/page.tsx
@@ -1,23 +1,86 @@
-import Link from 'next/link';
-import styles from './styles.module.css'
+// import Link from 'next/link';
+// import styles from './styles.module.css'
 
+
+// type Person = {
+//   id: number,
+//   name: string,
+//   films: any,
+//   starships: any,
+//   hair_color: any,
+//   gender: any,
+// };
+
+// type HeroProps = {
+//   id: string;
+//   params: any;
+// };
+
+// async function fetchData(id: string): Promise<Person> {
+//   const res = await fetch(`https://sw-api.starnavi.io/people/` + id);
+//   const result = await res.json();
+//   return {
+//     id: result.id,
+//     name: result.name,
+//     films: result.films,
+//     starships: result.starships,
+//     hair_color: result.hair_color,
+//     gender: result.gender,
+//   };
+// }
+// const Hero: React.FC<HeroProps> = async ({ params: {id} }) => {
+//   const person = await fetchData(id);
+
+//   return (
+//     <>
+//       <h1>{person.name}</h1>
+//       <div >
+//         <h2>Starships</h2>
+//       <ul className={styles.container}>
+//         {person.starships.map((starship: string, index: number) => (
+//           <li className={styles.containerChild} key={index}>{starship}</li>
+//         ))}
+//       </ul>
+//       <h2>Films</h2>
+//       <ul className={styles.container}>
+//         {person.films.map((film: string, index: number) => (
+//           <li className={styles.containerChild} key={index}>{film}</li>
+//         ))}
+//         </ul>
+//       </div>
+//             <Link className={styles.linkStyle} href="/">
+//         Назад на главную страницу
+//       </Link>
+//     </>
+//   );
+// };
+
+
+
+// export default Hero;
+
+
+import React from 'react';
+import Link from 'next/link';
+import styles from './styles.module.css';
 
 type Person = {
-  id: number,
-  name: string,
-  films: any,
-  starships: any,
-  hair_color: any,
-  gender: any,
+  id: number;
+  name: string;
+  films: any;
+  starships: any;
+  hair_color: any;
+  gender: any;
 };
 
-type HeroProps = {
-  id: string;
-  params: any;
+type PageProps = {
+  params: {
+    id: string;
+  };
 };
 
 async function fetchData(id: string): Promise<Person> {
-  const res = await fetch(`https://sw-api.starnavi.io/people/` + id);
+  const res = await fetch(`https://sw-api.starnavi.io/people/${id}`);
   const result = await res.json();
   return {
     id: result.id,
@@ -28,33 +91,33 @@ async function fetchData(id: string): Promise<Person> {
     gender: result.gender,
   };
 }
-const Hero: React.FC<HeroProps> = async ({ params: {id} }) => {
+
+const Hero = async ({ params }: PageProps) => {
+  const { id } = params;
   const person = await fetchData(id);
 
   return (
     <>
       <h1>{person.name}</h1>
-      <div >
+      <div>
         <h2>Starships</h2>
-      <ul className={styles.container}>
-        {person.starships.map((starship: string, index: number) => (
-          <li className={styles.containerChild} key={index}>{starship}</li>
-        ))}
-      </ul>
-      <h2>Films</h2>
-      <ul className={styles.container}>
-        {person.films.map((film: string, index: number) => (
-          <li className={styles.containerChild} key={index}>{film}</li>
-        ))}
+        <ul className={styles.container}>
+          {person.starships.map((starship: string, index: number) => (
+            <li className={styles.containerChild} key={index}>{starship}</li>
+          ))}
+        </ul>
+        <h2>Films</h2>
+        <ul className={styles.container}>
+          {person.films.map((film: string, index: number) => (
+            <li className={styles.containerChild} key={index}>{film}</li>
+          ))}
         </ul>
       </div>
-            <Link className={styles.linkStyle} href="/">
+      <Link className={styles.linkStyle} href="/">
         Назад на главную страницу
       </Link>
     </>
   );
 };
-
-
 
 export default Hero;
